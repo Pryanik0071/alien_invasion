1646ded3fdccd5c16273109851a3e77b1a532dc6
Dmitry <di.mesler.2010@yandex.ru>
2022-04-25 17:29:32 +0300

    Refactoring: move right - add functional for change rect.x, then right button is DOWN

--- alien_invasion.py
+++ alien_invasion.py
@@ -23,9 +23,8 @@ class AlienInvasion:
     def run_game(self):
         """Запуск основного цикла игры."""
         while True:
-            # Отслеживание событий клавиатуры и мыши.
             self._check_events()
-            # При каждом проходе цикла перерисовывается экран.
+            self.ship.update()
             self._update_screen()
 
     def _check_events(self):
@@ -35,8 +34,10 @@ class AlienInvasion:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    # Переместить корабль вправо
-                    self.ship.rect.x += 1
+                    self.ship.moving_right = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
 
     def _update_screen(self):
         """Обновляет изображения на экране и отображает новый экран"""
