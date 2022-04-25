9d6aa6f724c5330d169315ede8bdad17985369e9
Dmitry <di.mesler.2010@yandex.ru>
2022-04-25 21:44:57 +0300

    Add: close game - press ESC + add FULLSCREEN mode start

--- alien_invasion.py
+++ alien_invasion.py
@@ -14,6 +14,9 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
 
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")  # Title
@@ -44,6 +47,8 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
+            sys.exit()
+        elif event.key == pygame.K_ESCAPE:
             sys.exit()
 
     def _check_keyup_events(self, event):
