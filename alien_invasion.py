9209109adff116ce1179067d90802bc47479389f
Dmitry M <di.mesler.2010@yandex.ru>
2022-05-04 18:32:54 +0300

    Add: move fleet to the left + down

--- alien_invasion.py
+++ alien_invasion.py
@@ -85,7 +85,11 @@ class AlienInvasion:
                 self.bullets.remove(bullet)
 
     def _update_aliens(self):
-        """Обновление позиции всех пришельцев во флоте."""
+        """
+            Проверяет, достиг ли флот края экрана, с последующим
+            обновление позиции всех пришельцев во флоте.
+        """
+        self._check_fleet_edges()
         self.aliens.update()
 
     def _create_fleet(self):
@@ -117,6 +121,19 @@ class AlienInvasion:
         alien.rect.x = alien.x
         alien.rect.y = alien.rect.height + 2 * alien_height * row_number
         self.aliens.add(alien)
+
+    def _check_fleet_edges(self):
+        """Реагирует на достижение пришельцем края экрана."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """Опускает весь флот и меняет направление флота."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
     def _update_screen(self):
         """Обновляет изображения на экране и отображает новый экран"""
